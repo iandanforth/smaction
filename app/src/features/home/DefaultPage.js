8d5bf6e933e1f6c6b694814e6c596db4569452b4
Ian Danforth <iandanforth@gmail.com>
2018-07-30 13:16:58 -0700

    Get the rest working

--- app/src/features/home/DefaultPage.js
+++ app/src/features/home/DefaultPage.js
@@ -6,8 +6,10 @@ import Plot from 'react-plotly.js';
 import * as actions from './redux/actions';
 import Tooltip from 'rc-tooltip';
 import Slider, { Handle } from 'rc-slider';
+import { sum, min } from 'lodash';
 import 'rc-slider/assets/index.css';
 
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 export class DefaultPage extends Component {
   static propTypes = {
@@ -18,43 +20,87 @@ export class DefaultPage extends Component {
   constructor(props) {
     super(props);
 
+    const actionValues = [22.1, 150.0, -33.5, 0.01, 19.2];
+    const temperature = 100;
     const trace = this.getTrace(
-      [1, 2, 3],
-      [2, 5, 3]
+      actionValues,
+      temperature
     );
 
-    const layout = this.getLayout(1);
+    const initialRev = 1;
+    const layout = this.getLayout(initialRev);
 
     this.state = {
       data: [trace],
-      layout: { width: 640, height: 480, title: 'A Fancy Plot' },
+      layout: layout,
       frames: [],
       config: {},
-      revision: 1
+      revision: initialRev,
+      actionValues: actionValues,
+      temperature
     };
 
 
     this.updateTooltip = this.updateTooltip.bind(this);
-    this.handleSliderChange = this.handleSliderChange.bind(this);
+    this.handleYSliderChange = this.handleYSliderChange.bind(this);
+    this.handleTSliderChange = this.handleTSliderChange.bind(this);
     this.getLayout = this.getLayout.bind(this);
   }
 
-  getTrace(xs, ys) {
-     return { 
-      x: xs, 
-      y: ys,
+  makePositive(vals) {
+    const minVal = min(vals);
+    if (minVal >= 0) {
+      return vals
+    }
+    const offset = Math.abs(minVal);
+    let newVals = [];
+    for (let i=0; i<vals.length; i++){
+      const val = vals[i];
+      newVals.push(val + offset);
+    }
+    return newVals;
+  }
+
+  getTrace(actionValues, temperature) {
+    let xVals = []
+    let yVals = []
+
+    let tempYVals = [];
+    for (let i=0; i<actionValues.length; i++){
+      const val = actionValues[i];
+      tempYVals.push(Math.exp(val / temperature));
+      xVals.push(ALPHABET[i] + ': ' + val);
+    }
+
+    const tempSum = sum(tempYVals);
+    for (let i=0; i<tempYVals.length; i++){
+      const tempVal = tempYVals[i];
+      yVals.push(tempVal / tempSum);
+    }
+
+    return { 
+      x: xVals, 
+      y: yVals,
       type: 'bar', 
       marker: { 
         color: 'orange' },
-    }
+      }
   }
 
   getLayout(rev) {
     const layout = { 
       width: 640, 
       height: 480, 
-      title: 'A Fancy Plot',
-      datarevision: rev
+      title: 'Softmax Action Selection Probabilities',
+      datarevision: rev,
+      xaxis: {
+        categoryorder: "category ascending",
+        type: "category",
+        title: "Action name and value in an example state."
+      },
+      yaxis: {
+        title: "Probability"
+      }
     }
     return layout;
   }
@@ -74,25 +120,35 @@ export class DefaultPage extends Component {
     );
   };
 
-  handleSliderChange(val) {
-    console.log(this.state.data);
-    let { x, y } = this.state.data[0];
-    y[0] = val;
-    const newRev = this.state.revision + 1
-    const trace = this.getTrace(x, y);
+  handleTSliderChange(newTemperature) {
+    let { actionValues, revision } = this.state;
+    const newRev = revision + 1
+    const trace = this.getTrace(actionValues, newTemperature);
     const layout = this.getLayout(newRev)
     this.setState({
       data: [trace],
       layout: layout,
-      revision: newRev
+      revision: newRev,
+      temperature: newTemperature
+    });
+  }
+
+  handleYSliderChange(val) {
+    let { actionValues, temperature, revision } = this.state;
+    actionValues[0] = val;
+    const newRev = revision + 1
+    const trace = this.getTrace(actionValues, temperature);
+    const layout = this.getLayout(newRev)
+    this.setState({
+      data: [trace],
+      layout: layout,
+      revision: newRev,
+      actionValues
     });
   }
 
   render() {
-    const wrapperStyle = { width: 400, margin: 50 };
-
-    console.log(this.state)
-
+    const wrapperStyle = { width: 600, margin: "auto" };
     return (
       <div className="home-default-page">
         <div className="app-intro">
@@ -106,13 +162,23 @@ export class DefaultPage extends Component {
             onUpdate={figure => this.setState(figure)}
           />
           <div style={wrapperStyle}>
-            <p>Slider with custom handle</p>
+            <p>Change the softmax temperature</p>
+            <Slider 
+              min={1} 
+              max={2000}
+              step={1}
+              defaultValue={100}
+              handle={this.updateTooltip} 
+              onChange={this.handleTSliderChange}
+            />
+            <p>Change the value of State-Action pair 'a'</p>
             <Slider 
               min={0} 
-              max={20}
-              defaultValue={3}
+              max={200}
+              step={1}
+              defaultValue={100}
               handle={this.updateTooltip} 
-              onChange={this.handleSliderChange}
+              onChange={this.handleYSliderChange}
             />
           </div>
         </div>
